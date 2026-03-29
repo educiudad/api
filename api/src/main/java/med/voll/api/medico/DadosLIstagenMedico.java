9d9288d6d8e032f2d868144a8337b7319384e2b5
eduardo ciudad figueredo <eduardociudadfigueredo@gmail.com>
2026-03-29 14:39:59 -0300

    terminei o CRUD

--- api/src/main/java/med/voll/api/medico/DadosLIstagenMedico.java
+++ api/src/main/java/med/voll/api/medico/DadosLIstagenMedico.java
@@ -1,9 +1,9 @@
 package med.voll.api.medico;
 
-public record DadosLIstagenMedico(String nome,String email,  String crm, Especialidade especialidade) {
+public record DadosLIstagenMedico(Long id, String nome,String email,  String crm, Especialidade especialidade) {
 
     public DadosLIstagenMedico(Medico medico){
-        this(medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
+        this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
     }
 
 }
--- api/src/main/java/med/voll/api/medico/MedicoRepository.java
+++ api/src/main/java/med/voll/api/medico/MedicoRepository.java
@@ -1,8 +1,11 @@
 package med.voll.api.medico;
-
+import org.springframework.data.domain.Page;
+import io.micrometer.observation.ObservationFilter;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
 
 
+    Page<Medico> findAllByAtivoTrue(Pageable paginacao);
 }
